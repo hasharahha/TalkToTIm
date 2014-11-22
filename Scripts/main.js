42a1ca0991131eb7e1474def89ff009166aeaf3a
Jacob Bondley <hasharahha@gmail.com>
2014-11-22 09:58:12 -0500

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,15 +2,27 @@ var data =[];
 var cache;
 var Cmd;
 var Responces =[
-"
+"Hello to you sir!",
+"good morning to you to!"
+"Good afternoon to you!",
+"Please don't curse!"
+];
+function log(msg){
+  confirm("Tim: \n"+msg);
+  data[0] = msg;
+  
+}
 
 function ProcCmd(){
 var command = document.getElementById("command").value;
 Cmd = command.split(" ");
-var data = Cmd.toLowerCase();
+cache = Cmd.toLowerCase();
 
-if(data[0]=="hello"){
-
+if(cache[0]=="hello"){
+log(Responces[0]);
+}
+else if(cache[0]=="good"&&cache[1]=="morning"){
+  
 }
 
 
