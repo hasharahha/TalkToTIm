75d1089fd9531cf8610f3ec2547a391b5c9577a9
Jacob Bondley <hasharahha@gmail.com>
2014-11-22 09:51:26 -0500

    Create main.js

---
+++ Scripts/main.js
@@ -0,0 +1,18 @@
+var data =[];
+var cache;
+var Cmd;
+var Responces =[
+"
+
+function ProcCmd(){
+var command = document.getElementById("command").value;
+Cmd = command.split(" ");
+var data = Cmd.toLowerCase();
+
+if(data[0]=="hello"){
+
+}
+
+
+}
+
